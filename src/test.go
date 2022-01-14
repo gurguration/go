b3c805ce37da2eb5b0f11cf2ba94bb31df749345
gurguration <g.zakareishvili@gmail.com>
2022-01-14 17:21:02 +0400

    variable cannot be declared twice on the same block

--- src/test.go
+++ src/test.go
@@ -5,7 +5,25 @@ import "fmt"
 func main() {
 	i := 1
 	i = 34
-	var my_name string = "guram"
+	var my_name string = "\nGuram"
 	fmt.Println(i)
-	fmt.Println(my_name)
+	myLoop()
+	fmt.Printf("%v, %T", my_name, my_name)
 }
+
+var loopvar int = 10
+
+func myLoop() {
+	var (
+		actorName = 1
+	)
+	fmt.Printf("%v ", actorName)
+	for i := loopvar; i < loopvar; i++ {
+		println(i)
+	}
+}
+
+var (
+	actorName string = "Elisabeth"
+	season    int    = 11
+)
