4179852dad5b1db70692f7a02dbd4b561b4b63ce
gurguration <g.zakareishvili@gmail.com>
2022-01-18 23:26:42 +0400

    Defer, Panic, Recover: Defer closing res.Body object

--- src/test.go
+++ src/test.go
@@ -2,46 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
-
-const (
-	_   = iota      // Iota initializer must be constant
-	one = 1 << iota // m
-	two
-	three
-	four
-)
-
-const (
-	t        = 3
-	y        = 'a'
-	u        = "string"
-	i uint64 = 53 + t // constants can be interpolated unlike variables
-)
-
-const (
-	readonly = 1 << iota
-	x1
-	service
-	x2
-	admin
-)
-
-type Doctor struct {
-	number     int
-	specialty  string `required max: "10"`
-	companions string
-}
-
-type Specialist struct {
-	Doctor
-	isSpecialist bool
-}
 
 func main() {
-	i := 0
-	for ; i < 15; i++ {
-		fmt.Println("This is i:", i)
-		i = i + 2
+	res, err := http.Get("https://google.com/robots.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer res.Body.Close()
+	robots, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Retrieved robots txt: %v: \n", string(robots))
 }
