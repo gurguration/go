3caba88ee0b41320253972e3b0457fc921cf8335
gurguration <g.zakareishvili@gmail.com>
2022-01-19 21:41:56 +0400

    Functions: Returning pointers

--- src/test.go
+++ src/test.go
@@ -6,13 +6,13 @@ import (
 
 func main() {
 	s := sum(1, 22, 33, 2, 3, 41, 94)
-	fmt.Println("The sum is:", s)
+	fmt.Println("The sum is:", *s)
 }
 
-func sum(numbers ...int) int {
+func sum(numbers ...int) *int {
 	sum := 0
 	for _, i := range numbers {
 		sum += i
 	}
-	return sum
+	return &sum
 }
