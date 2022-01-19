073f7ca91ffef7140029baf629d0cb6a27de592f
gurguration <g.zakareishvili@gmail.com>
2022-01-19 18:21:30 +0400

    Looping: Range over collections

--- src/test.go
+++ src/test.go
@@ -5,19 +5,8 @@ import (
 )
 
 func main() {
-	i := 0
-	fmt.Println("Broke outside loop")
-outerLoop:
-	for i < 10 {
-		i++
-		for j := 0; j < 10; j++ {
-			// fmt.Printf("i IS: %v\n", i)
-			fmt.Printf("i IS: %v and this is J: %v\n", i, j)
-			if j == 3 {
-				continue outerLoop
-				// break outerLoop
-			}
-		}
-
+	s := []int{1821, 3332, 83, 34, 6}
+	for k, v := range s {
+		fmt.Printf("This is key %v, This is V: %v\n", k, v)
 	}
 }
