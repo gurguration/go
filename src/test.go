220b443d2b8f66c27e558548c25f8c30fa86ef03
gurguration <g.zakareishvili@gmail.com>
2022-01-19 16:03:05 +0400

    Looping: Break and Continue

--- src/test.go
+++ src/test.go
@@ -6,8 +6,18 @@ import (
 
 func main() {
 	i := 0
+	fmt.Println("Broke outside loop")
+outerLoop:
 	for i < 10 {
-		fmt.Printf("i IS: %v\n", i)
 		i++
+		for j := 0; j < 10; j++ {
+			// fmt.Printf("i IS: %v\n", i)
+			fmt.Printf("i IS: %v and this is J: %v\n", i, j)
+			if j == 3 {
+				continue outerLoop
+				// break outerLoop
+			}
+		}
+
 	}
 }
