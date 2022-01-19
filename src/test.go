397513b1702e22b3545886ab4eaa1c788a9def56
gurguration <g.zakareishvili@gmail.com>
2022-01-19 21:49:06 +0400

    Functions: Confusing syntax initialize return value in the signature of function

--- src/test.go
+++ src/test.go
@@ -6,13 +6,12 @@ import (
 
 func main() {
 	s := sum(1, 22, 33, 2, 3, 41, 94)
-	fmt.Println("The sum is:", *s)
+	fmt.Println("The sum is:", s)
 }
 
-func sum(numbers ...int) *int {
-	sum := 0
+func sum(numbers ...int) (result int) {
 	for _, i := range numbers {
-		sum += i
+		result += i
 	}
-	return &sum
+	return
 }
