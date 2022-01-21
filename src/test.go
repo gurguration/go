c6c6d97a0a9765169eb5773c1f5cfbbd073ec213
gurguration <g.zakareishvili@gmail.com>
2022-01-21 22:39:02 +0400

    Channels: Go routine directional channels

--- src/test.go
+++ src/test.go
@@ -2,37 +2,22 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"sync"
 )
 
-// WAIT GROUP
 var wg = sync.WaitGroup{}
-var counter = 0
-
-// MUTEX
-var m = sync.RWMutex{}
 
 func main() {
-	runtime.GOMAXPROCS(100)
-	for i := 0; i < 10; i++ {
-		wg.Add(2)
-		m.RLock()
-		go sayHello()
-		m.Lock()
-		go increment()
-	}
+	ch := make(chan int)
+	wg.Add(2)
+	go func(ch chan<- int) {
+		ch <- 53
+		wg.Done()
+	}(ch)
+	go func(ch <-chan int) {
+		i := <-ch
+		fmt.Println(i)
+		wg.Done()
+	}(ch)
 	wg.Wait()
-}
-
-func sayHello() {
-	fmt.Printf("Hello %v \n", counter)
-	m.RUnlock()
-	wg.Done()
-}
-
-func increment() {
-	counter++
-	m.Unlock()
-	wg.Done()
 }
