9e26092967b65d33c690762b2b5d4df3aa0e4986
gurguration <g.zakareishvili@gmail.com>
2022-01-22 18:19:49 +0400

    Interfaces: Classsic shapes example

--- src/test.go
+++ src/test.go
@@ -1,60 +1,32 @@
 package main
 
-import "log"
+import "fmt"
 
-type subsciber struct {
-	subids string
-}
-
-func (s subsciber) id() string {
-	return s.subids
-}
-func (s subsciber) react(msg string) {
-	log.Printf("ID %s - received: %s", s.id(), msg)
+type shape interface {
+	area() float64
+	perimeter() float64
 }
 
-func (p publisher) removeSubscriber(subid string) {
-	delete(p.subscribers, subid)
-}
-func newSubscriber(id string) subsciber {
-	return subsciber{subids: id}
+type rectangle struct {
+	width, heith float64
 }
 
-type Subscriber interface {
-	id() string
-	react(msg string)
-}
-type publisher struct {
-	subscribers map[string]Subscriber
+func (r rectangle) perimeter() float64 {
+	return r.heith * r.width / 2
 }
 
-func (p publisher) addSubscriber(subscriber Subscriber) {
-	p.subscribers[subscriber.id()] = subscriber
-}
-func (p publisher) broadcast(msg string) {
-	for _, v := range p.subscribers {
-		v.react(msg)
-	}
+func (r rectangle) area() float64 {
+	result := r.heith * r.width / 2
+	fmt.Println(result)
+	return result
 }
 
-func newPublisher() publisher {
-	return publisher{make(map[string]Subscriber)}
-}
-
-type Publisher interface {
-	broadcast(msg string)
-	addSubscriber(subcriber Subscriber)
-	removeSubscriber(id string)
+func Calculate(s shape) float64 {
+	result := s.area()
+	return result
 }
 
 func main() {
-	var p Publisher
-	p = newPublisher()
-	p.broadcast("hello")
-	s := newSubscriber("23")
-	s2 := newSubscriber("444")
-	p.addSubscriber(s)
-	p.addSubscriber(s2)
-	p.removeSubscriber("23")
-	p.broadcast("helllo")
+	rshape := rectangle{width: 1.2, heith: 33.22}
+	Calculate(rshape)
 }
