3986b91a7186a48cc273fedfb1a13a0d8158c55f
gurguration <g.zakareishvili@gmail.com>
2022-01-23 19:59:16 +0400

    Building Error with struct with extra properties

--- src/test.go
+++ src/test.go
@@ -1,37 +1,18 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
-	"time"
-	// "strconv"
-	// "../car"
 )
 
-func main() {
-	cn := context.Background()
-	_ = context.WithValue(cn, "myKey", 123)
-	// t := time.Now().Add(1 * time.Second)
-	// ctx2, _ := context.WithDeadline(cn, t)
-	// deadline, ok := ctx2.Deadline()
-	// fmt.Println(deadline)
-	// log.Print(ok)
-	timeout := 5 * time.Second
-	ctx2, _ := context.WithTimeout(cn, timeout)
-	// fmt.Println(ctx2.Err())
-	time.Sleep(3 * time.Second)
-	fmt.Println(ctx2)
-	fn(cn)
+type MyError struct {
+	extraInfo string
 }
 
-func fn(ctx context.Context) {
-	select {
-	case <-time.After(2 * time.Second):
-		log.Print("After two Sec")
-	case <-time.After(1 * time.Second):
-		log.Print("After one Sec")
-	case <-ctx.Done():
-		log.Print("Boom context is done")
-	}
+func (e *MyError) Error() string {
+	return fmt.Sprintf("got an error: %s", e.extraInfo)
+}
+func main() {
+	var e error
+	e = &MyError{extraInfo: "SSSRrrrra, BOOM!!!"}
+	fmt.Println(e)
 }
